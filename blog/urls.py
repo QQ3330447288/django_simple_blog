08d05509510d5fd670d46a12cf897a596064d1d1
Thanlon <thanlon@sina.com>
2019-05-10 16:42:48 +0800

    Complete Article's detail page

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ import blog.views
 urlpatterns = [
     path('lst', blog.views.lst),
     path('content', blog.views.article_content),
-    path('index', blog.views.get_index_page)
+    path('index', blog.views.get_index_page),
+    # path('detail', blog.views.get_detail_page)
+    path('detail/<int:article_id>', blog.views.get_detail_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -29,3 +29,36 @@ def get_index_page(request):
                   {
                       'article_list': all_article
                   })
+
+
+def get_detail_page(request, article_id):
+    all_article = Article.objects.all()
+    print(type(all_article))
+    print(all_article)
+    curr_article = None
+    previous_index = 0
+    next_index = 0
+    previous_article = None
+    next_article = None
+    for index, article in enumerate(all_article):
+        if index == 0:
+            previous_index = 0
+            next_index = index + 1
+        elif index == len(all_article) - 1:
+            previous_index = index - 1
+            next_index = index
+        else:
+            previous_index = index - 1
+            next_index = index + 1
+        if article.article_id == article_id:
+            curr_article = article
+            previous_article = all_article[previous_index]
+            next_article = all_article[next_index]
+            break
+    section_list = curr_article.content.split('\n')
+    return render(request, 'blog/detail.html', {
+        'curr_article': curr_article,
+        'section_list': section_list,
+        'previous_article': previous_article,
+        'next_article': next_article
+    })
