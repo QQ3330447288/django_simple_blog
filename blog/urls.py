40b92943f6d01a2f38f0ef5d965251983d4c7df8
Thanlon <thanlon@sina.com>
2019-05-10 14:54:50 +0800

    Complete blog's home page

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ import blog.views
 
 urlpatterns = [
     path('lst', blog.views.lst),
+    path('content', blog.views.article_content),
+    path('index', blog.views.get_index_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 
 
 # Create your views here.
 def lst(request):
     return HttpResponse('Hello World')
+
+
+# 视图函数
+def article_content(request):
+    article = Article.objects.all()[0]
+    article_id = article.article_id
+    title = article.title
+    brief_content = article.brief_content
+    content = article.content
+    publish_date = article.date
+    return_str = 'article_id:%s,title:%s,brief_content:%s,' \
+                 'content:%s,publish_date:%s' % (article_id, title,
+                                                 brief_content, content,
+                                                 publish_date)
+    return HttpResponse(return_str)
+
+
+def get_index_page(request):
+    all_article = Article.objects.all()
+    return render(request, 'blog/index.html',
+                  {
+                      'article_list': all_article
+                  })
